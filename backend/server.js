3e911cca87d001820ff6c9af91e2b4002381ba6c
Fiona <fionawu@genius-bull.com>
2021-01-21 17:31:45 +0800

    deploy on heroku

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,25 @@
+const express = require("express");
+const path = require("path");
+const port = process.env.PORT || 80;
+const app = express();
+app.use(express.static(path.join(__dirname, "build")));
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+app.get("/ping", function (req, res) {
+    return res.send("pong");
+});
+
+app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(port);
+console.log("Server Ready!");
+
+//==========================================================================
+
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 require("dotenv-defaults").config();
